00bd745ae67156de9c22728c969694bbdd1f83b7
Mark Holdsworth <mholdsworth@me.com>
2025-12-01 16:52:36 +0000

    Initial Commit

---
+++ Fasting and Water Tracker/Fasting_and_Water_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fasting_and_Water_TrackerApp.swift
+//  Fasting and Water Tracker
+//
+//  Created by Mark Holdsworth on 01/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Fasting_and_Water_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
